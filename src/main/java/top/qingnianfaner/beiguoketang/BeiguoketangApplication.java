24ebb951910c8b7884b9999f174f5f180fe3ca8d
BeiGuo <beiguo@BeiGuodeMacBook-Pro.local>
2018-05-11 15:49:32 +0800

    Initial commit

---
+++ src/main/java/top/qingnianfaner/beiguoketang/BeiguoketangApplication.java
@@ -0,0 +1,12 @@
+package top.qingnianfaner.beiguoketang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeiguoketangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeiguoketangApplication.class, args);
+	}
+}
